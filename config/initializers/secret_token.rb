c560f1137c6f1666c462244aa000854d5747ed90
App Academy Student <student@appacademy.io>
2014-10-02 09:01:40 -0400

    Initial commit. Start Rails project and generate User model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicCatalog::Application.config.secret_key_base = '4b691dab5d6ed7252da21364dcd9a747245c82d2899ed53da6f970a2b475eeb4457c7bc4eb80f09f2061aac611b22f4d47db89694a0ef606f3d95fd43292509d'
